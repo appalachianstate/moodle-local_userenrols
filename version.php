28c7c7a171e467bfff6f28c49685a3620e321cfc
Fred Woolard <woolardfa@appstate.edu>
2016-03-16 14:07:56 -0400

    Ver. 2013052005 Release 0.0.5 (Build 20160316)

--- version.php
+++ version.php
@@ -34,9 +34,9 @@
 
     $plugin             = new stdClass();
 
-    $plugin->version    = 2013052004;
-    $plugin->requires   = 2012062500;
-    $plugin->release    = "0.0.4";
+    $plugin->version    = 2013052005;
+    $plugin->requires   = 2015051100;
+    $plugin->release    = "0.0.5 (Build 20160316)";
     $plugin->component = 'local_userenrols';
     $plugin->cron       = 0;
     $plugin->maturity   = MATURITY_STABLE;
